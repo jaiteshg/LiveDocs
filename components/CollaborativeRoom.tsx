2a1b99cdc4935d21fca98f6000927fee3026a606
jaiteshg <gjaitesh@gmail.com>
2026-03-23 19:25:32 +0530

    feat: implement collaborative document editing with user authentication, add loading and error handling components

---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import { RoomProvider, ClientSideSuspense } from "@liveblocks/react/suspense";
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import ActiveCollaborators from "./ActiveCollaborators";
+
+const CollaborativeRoom = ({ roomId, roomMetadata}: CollaborativeRoomProps) => {
+  return (
+    <RoomProvider id={roomId}>
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title">Share</p>
+            </div>
+            <div className="flex w-full flex-1 justify-end gap-2">
+              <ActiveCollaborators/>
+              <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            </div>
+          </Header>
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
