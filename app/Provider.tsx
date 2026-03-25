c46a76f6d493edbbaee16ce919315c7d416472de
jaiteshg <gjaitesh@gmail.com>
2026-03-25 17:48:18 +0530

    feat: update ShareModal to handle document access updates; improve Header and UserTypeSelector formatting; add Theme plugin for editor styling

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { LiveblocksProvider, ClientSideSuspense} from "@liveblocks/react/suspense"
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,11 @@
+import { cn } from "@/lib/utils"
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { cn } from '@/lib/utils'
 
 const Header = ({ children, className }: HeaderProps) => {
   return (
-    <div className={cn("header", className)}>
+    <div className={cn('header', className)}>
         <Link href="/" className="md:flex-1">
         <Image 
         src="/assets/icons/logo.svg"
